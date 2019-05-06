493dd4d9862b7aa4ca693a86ada80b6e6ec05897
Cristian Harsu <charsu@stratag.com>
2019-05-06 09:44:26 +0100

    adding missing files

---
+++ Plain.Courier.Core/Delivery/IParcelDeliveryCostRule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plain.Courier.Core.Delivery.Models;
+
+namespace Plain.Courier.Core.Delivery {
+   public interface IParcelDeliveryCostRule {
+      DeliveryTotal GetCost(Order order, Parcel parcel);
+   }
+}
---
+++ Plain.Courier.Core/Delivery/Services/Rules/SimpleParcelDeliveryCostRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plain.Courier.Core.Delivery.Models;
+
+namespace Plain.Courier.Core.Delivery.Services.Rules {
+   public class SimpleParcelDeliveryCostRule : IParcelDeliveryCostRule {
+      public DeliveryTotal GetCost(Order order, Parcel parcel) {
+         throw new NotImplementedException();
+      }
+   }
+}
